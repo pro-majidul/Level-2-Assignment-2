d3d6ac2355e6e4d8efbe2366c591702bef136cfc
Md Majidul Islam <majidul123tub@gmail.com>
2026-04-23 14:06:22 +0600

    database connected

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,31 @@
 import express, { Request, Response } from 'express'
+import { Pool } from 'pg'
+
 const app = express()
 const port = 5000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const pool = new Pool({
+    connectionString: `postgresql://neondb_owner:npg_1VykE6eXGxrY@ep-fancy-surf-anzd1nc3-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+})
+
+const initDB = async () => {
+    await pool.query(`CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        age INT ,
+        phone VARCHAR(15) NOT NULL,
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW(),
+    )`)
+}
+
+initDB()
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
