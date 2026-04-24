ee8d56990b1c7f639dfb108c832a8cf297538fa0
Md Majidul Islam <majidul123tub@gmail.com>
2026-04-24 16:30:56 +0600

    dotenv file setup added

--- src/server.ts
+++ src/server.ts
@@ -1,57 +1,60 @@
 import express, { Request, Response } from 'express'
 import { Pool } from 'pg'
+import dotenv from 'dotenv'
+import path from 'path'
 
+dotenv.config({ path: path.join(process.cwd(), '.env') })
 const app = express()
-const port = 5000
+const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 const pool = new Pool({
-    connectionString: `postgresql://neondb_owner:npg_1VykE6eXGxrY@ep-fancy-surf-anzd1nc3-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+    connectionString: `${process.env.CONNECTION_STRING}`,
 })
 
 const initDB = async () => {
-    await pool.query(`CREATE TABLE IF NOT EXISTS users (
+    await pool.query(`CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL UNIQUE,
-        password VARCHAR(255) NOT NULL,
-        phone VARCHAR(15) NOT NULL,
-        role VARCHAR(50) NOT NULL CHECK (role IN ('admin', 'user')),
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
+        password VARCHAR(255) NOT NULL CHECK (LENGTH(password) >= 6),
+        phone VARCHAR(20) NOT NULL,
+        role VARCHAR(10) NOT NULL CHECK (role IN ('admin', 'customer'))
     )`);
 
-    await pool.query(`CREATE TABLE IF NOT EXISTS vehicles (
-    id SERIAL PRIMARY KEY,
-    vehicle_name VARCHAR(255) NOT NULL,
-    type VARCHAR(10) CHECK (type IN ('car', 'bike', 'van', 'SUV')) NOT NULL,
-    registration_number VARCHAR(100) UNIQUE NOT NULL,
-    daily_rent_price DECIMAL(10, 2) NOT NULL CHECK (daily_rent_price > 0),
-    availability_status VARCHAR(10) CHECK (availability_status IN ('available', 'booked')) NOT NULL DEFAULT 'available'
+    await pool.query(`CREATE TABLE IF NOT EXISTS vehicles(
+        id SERIAL PRIMARY KEY,
+        vehicle_name VARCHAR(255) NOT NULL,
+        type VARCHAR(10) NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+        registration_number VARCHAR(255) UNIQUE NOT NULL,
+        daily_rent_price NUMERIC(10,2) NOT NULL CHECK(daily_rent_price > 0),
+        availability_status VARCHAR(50) NOT NULL CHECK(availability_status IN ('available', 'booked'))
     )`);
-
 
     await pool.query(`CREATE TABLE IF NOT EXISTS bookings(
         id SERIAL PRIMARY KEY,
-        customer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-        vehicle_id INTEGER REFERENCES vehicles(id) ON DELETE CASCADE,
+        customer_id INT NOT NULL REFERENCES users(id),
+        vehicle_id INT NOT NULL REFERENCES vehicles(id),
         rent_start_date DATE NOT NULL,
-        rent_end_date DATE NOT NULL,
-        total_price DECIMAL(10, 2) NOT NULL CHECK (total_price >= 0),
-        status VARCHAR(10) CHECK (status IN ('active', 'cancelled', 'returned')) NOT NULL DEFAULT 'active',
-        CONSTRAINT valid_dates CHECK (rent_end_date >= rent_start_date)
-        )`)
-
+        rent_end_date DATE NOT NULL CHECK (rent_end_date > rent_start_date),
+        total_price NUMERIC(10,2) NOT NULL CHECK (total_price > 0),
+        status VARCHAR(50) NOT NULL CHECK (status IN ('active','cancelled','returned'))
+    )`);
 }
+
 
 initDB()
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+
+app.get("/", (req: Request, res: Response) => {
+    res.send({
+        status: true,
+        message: 'server is running.........!'
+    })
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`The server is running on port ${port}`)
 })
