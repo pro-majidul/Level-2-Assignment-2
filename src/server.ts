d866f715edd76aa748a3c469d6dcfed29cba01f9
Md Majidul Islam <majidul123tub@gmail.com>
2026-04-24 15:39:05 +0600

    Data table added

--- src/server.ts
+++ src/server.ts
@@ -16,16 +16,37 @@ const initDB = async () => {
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL UNIQUE,
-        age INT ,
+        password VARCHAR(255) NOT NULL,
         phone VARCHAR(15) NOT NULL,
-        address TEXT,
+        role VARCHAR(50) NOT NULL CHECK (role IN ('admin', 'user')),
         created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW(),
-    )`)
+        updated_at TIMESTAMP DEFAULT NOW()
+    )`);
+
+    await pool.query(`CREATE TABLE IF NOT EXISTS vehicles (
+    id SERIAL PRIMARY KEY,
+    vehicle_name VARCHAR(255) NOT NULL,
+    type VARCHAR(10) CHECK (type IN ('car', 'bike', 'van', 'SUV')) NOT NULL,
+    registration_number VARCHAR(100) UNIQUE NOT NULL,
+    daily_rent_price DECIMAL(10, 2) NOT NULL CHECK (daily_rent_price > 0),
+    availability_status VARCHAR(10) CHECK (availability_status IN ('available', 'booked')) NOT NULL DEFAULT 'available'
+    )`);
+
+
+    await pool.query(`CREATE TABLE IF NOT EXISTS bookings(
+        id SERIAL PRIMARY KEY,
+        customer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+        vehicle_id INTEGER REFERENCES vehicles(id) ON DELETE CASCADE,
+        rent_start_date DATE NOT NULL,
+        rent_end_date DATE NOT NULL,
+        total_price DECIMAL(10, 2) NOT NULL CHECK (total_price >= 0),
+        status VARCHAR(10) CHECK (status IN ('active', 'cancelled', 'returned')) NOT NULL DEFAULT 'active',
+        CONSTRAINT valid_dates CHECK (rent_end_date >= rent_start_date)
+        )`)
+
 }
 
 initDB()
-
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
