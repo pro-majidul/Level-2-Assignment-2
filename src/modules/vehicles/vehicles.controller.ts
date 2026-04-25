bcff68475c6dbaa339352a2b5e808bf80be8a9e6
Md Majidul Islam <majidul123tub@gmail.com>
2026-04-25 13:30:05 +0600

    vehicles services data inserted in moduler patter added

--- src/modules/vehicles/vehicles.controller.ts
+++ src/modules/vehicles/vehicles.controller.ts
@@ -24,9 +24,8 @@ const getSingleVehicles = async (req: Request, res: Response) => {
 }
 
 const postVehicles = async (req: Request, res: Response) => {
-    const { vehicle_name, type, registration_number, daily_rent_price, availability_status } = req.body;
     try {
-        const result = await pool.query(`INSERT INTO vehicles(vehicle_name,type,registration_number,daily_rent_price,availability_status) VALUES($1,$2,$3, $4,$5) RETURNING *`, [vehicle_name, type, registration_number, daily_rent_price, availability_status])
+        const result = await vehicleServices.postVehicles(req.body)
         res.status(201).json({
             success: true,
             message: "Data Instered Successfully",
--- src/modules/vehicles/vehicles.service.ts
+++ src/modules/vehicles/vehicles.service.ts
@@ -5,4 +5,12 @@ const getAllVehicles = async () => {
     return result
 }
 
-export const vehicleServices = { getAllVehicles }
+const postVehicles = async (payload: Record<string, unknown>) => {
+    const { vehicle_name, type, registration_number, daily_rent_price, availability_status } = payload;
+    const result = await pool.query(`INSERT INTO vehicles(vehicle_name,type,registration_number,daily_rent_price,availability_status) VALUES($1,$2,$3, $4,$5) RETURNING *`, [vehicle_name, type, registration_number, daily_rent_price, availability_status])
+
+    return result
+
+}
+
+export const vehicleServices = { getAllVehicles, postVehicles }
